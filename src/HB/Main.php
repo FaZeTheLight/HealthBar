57a5f066534d070b3b2c21b19e4f07e52d1e90fe
Logan Hoffman <fazethelight@gmail.com>
2016-01-14 16:36:25 -0500

    Update main.php - Fixed Parsing Error

--- src/HB/Main.php
+++ src/HB/Main.php
@@ -29,67 +29,67 @@ if($this->getServer()->getPluginManager()->getPlugin("PurePerms") === null){
 $player->setNameTag($player->getName()."\n".($player->getHealth() / $player->getMaxHealth() * 100)."% PP NOT INSTALLED");
 }else{
 	if($health === 20){
-	$player->setNameTag("[".$groupname."]" .$player->getName()."\n§2||||||||||";
+	$player->setNameTag("[".$groupname."]" .$player->getName()."\n§2||||||||||");
 	}
 	if($health === 19){
-	$player->setNameTag("[".$groupname."]" .$player->getName()."\n§2||||||||||";
+	$player->setNameTag("[".$groupname."]" .$player->getName()."\n§2||||||||||");
 	}
 	if($health === 18){
-	$player->setNameTag("[".$groupname."]" .$player->getName()."\n§2|||||||||§8|";
+	$player->setNameTag("[".$groupname."]" .$player->getName()."\n§2|||||||||§8|");
 	}
 	if($health === 17){
-	$player->setNameTag("[".$groupname."]" .$player->getName()."\n§a|||||||||§8|";
+	$player->setNameTag("[".$groupname."]" .$player->getName()."\n§a|||||||||§8|");
 	}
 	if($health === 16){
-	$player->setNameTag("[".$groupname."]" .$player->getName()."\n§a|||||||||§8||";
+	$player->setNameTag("[".$groupname."]" .$player->getName()."\n§a|||||||||§8||");
 	}
 	if($health === 15){
-	$player->setNameTag("[".$groupname."]" .$player->getName()."\n§a|||||||||§8||";
+	$player->setNameTag("[".$groupname."]" .$player->getName()."\n§a|||||||||§8||");
 	}
 	if($health === 14){
-	$player->setNameTag("[".$groupname."]" .$player->getName()."\n§e|||||||§8|||";
+	$player->setNameTag("[".$groupname."]" .$player->getName()."\n§e|||||||§8|||");
 	}
 	if($health === 13){
-	$player->setNameTag("[".$groupname."]" .$player->getName()."\n§e|||||||§8|||";
+	$player->setNameTag("[".$groupname."]" .$player->getName()."\n§e|||||||§8|||");
 	}
 	if($health === 12){
-	$player->setNameTag("[".$groupname."]" .$player->getName()."\n§e||||||§8||||";
+	$player->setNameTag("[".$groupname."]" .$player->getName()."\n§e||||||§8||||");
 	}
 	if($health === 11){
-	$player->setNameTag("[".$groupname."]" .$player->getName()."\n§6||||||§8||||";
+	$player->setNameTag("[".$groupname."]" .$player->getName()."\n§6||||||§8||||");
 	}
 	if($health === 10){
-	$player->setNameTag("[".$groupname."]" .$player->getName()."\n§6|||||§8|||||";
+	$player->setNameTag("[".$groupname."]" .$player->getName()."\n§6|||||§8|||||");
 	}
         if($health === 9){
-	$player->setNameTag("[".$groupname."]" .$player->getName()."\n§6|||||§8|||||";
+	$player->setNameTag("[".$groupname."]" .$player->getName()."\n§6|||||§8|||||");
 	}
 	if($health === 8){
-	$player->setNameTag("[".$groupname."]" .$player->getName()."\n§c||||§8||||||";
+	$player->setNameTag("[".$groupname."]" .$player->getName()."\n§c||||§8||||||");
 	}
 	if($health === 7){
-	$player->setNameTag("[".$groupname."]" .$player->getName()."\n§c||||§8||||||";
+	$player->setNameTag("[".$groupname."]" .$player->getName()."\n§c||||§8||||||");
 	}
 	if($health === 6){
-	$player->setNameTag("[".$groupname."]" .$player->getName()."\n§c|||§8|||||||";
+	$player->setNameTag("[".$groupname."]" .$player->getName()."\n§c|||§8|||||||");
 	}
 	if($health === 5){
-	$player->setNameTag("[".$groupname."]" .$player->getName()."\n§4|||§8|||||||";
+	$player->setNameTag("[".$groupname."]" .$player->getName()."\n§4|||§8|||||||");
 	}
 	if($health === 4){
-	$player->setNameTag("[".$groupname."]" .$player->getName()."\n§4||§8||||||||";
+	$player->setNameTag("[".$groupname."]" .$player->getName()."\n§4||§8||||||||");
 	}
 	if($health === 3){
-	$player->setNameTag("[".$groupname."]" .$player->getName()."\n§4|§8|||||||||";
+	$player->setNameTag("[".$groupname."]" .$player->getName()."\n§4|§8|||||||||");
 	}
 	if($health === 2){
-	$player->setNameTag("[".$groupname."]" .$player->getName()."\n§5|§8|||||||||";
+	$player->setNameTag("[".$groupname."]" .$player->getName()."\n§5|§8|||||||||");
 	}
 	if($health === 1){
-	$player->setNameTag("[".$groupname."]" .$player->getName()."\n§8||||||||||";
+	$player->setNameTag("[".$groupname."]" .$player->getName()."\n§8||||||||||");
 	}
 	if($health === 0){
-	$player->setNameTag("[".$groupname."]" .$player->getName()."\n§5||||||||||§8";
+	$player->setNameTag("[".$groupname."]" .$player->getName()."\n§8||||||||||");
 	}
 }
 }
